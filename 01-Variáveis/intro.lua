c69df06666995c7876ca787538378df589d6eb67
Gustavo Luiz Gordoni <gustavogordoni1@gmail.com>
2025-08-17 18:12:14 -0300

    03-intro

---
+++ 01-Variáveis/intro.lua
@@ -0,0 +1,9 @@
+nome = "Gustavo" -- string
+saldo = 999 -- number
+nota = 99.99 -- number
+isAuth = false  -- boolean
+-- nil -- valor não associado
+
+
+print('Oi', nome)
+print('Você tem ', saldo,  'reais.')
