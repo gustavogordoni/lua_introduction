af6040cc88eed6011a9ebaf3833dc55f6f9b62dc
Gustavo Luiz Gordoni <gustavogordoni1@gmail.com>
2025-08-17 18:12:32 -0300

    04-indetificadores

---
+++ 01-Variáveis/identificadores.lua
@@ -0,0 +1,43 @@
+--[[
+
+  LINK: https://www.lua.org/manual/5.2/pt/manual.html#3.1
+
+  Nomes de variáveis ou identificadores válidos
+
+  Somente começam com letras ou _
+  Não podem ter espaços nem caracteres especiais
+
+  -- Exemplos válidos
+
+  myVar   MYNAME  _HELLO  _hi
+
+  ddafsbdsgdfdsfgdf a13g45g3jk4g534
+
+  i1 z45675 AND
+
+  -- Explos invoking
+  co-body   2d  3d  alguma variavel
+
+  árvore  e?sd  true
+
+  ----------- Palavras reservadas -----------
+  and       break       do        else       elseif       end
+  false     for         function  got        if           in
+  local     nill        nota      or         repeat       return
+  then      true        until     while      
+
+
+  ----------- Case sensitive -----------
+  Lua é uma linguagem que diferencia minúsculas de maiúsculas: and é uma palavra reservada, mas And e AND são dois nomes válidos diferentes. 
+
+  
+  ----------- Palavras reservadas -----------
+  Como uma convenção, nomes começando com um sublinhado seguido por letras maiúsculas (tais como _VERSION) são reservados para variáveis usadas por Lua.
+
+  Variáveis que não vão ter o valor modificado, são conhecidas como **constantes** e são representadas com letras maiúsculas como CONSTANTE oi PI ou SEI_LA_O_QUE
+
+  Podemos nomear variáveis seguindo alguns padrões
+    camelCase: nomeComposto
+    snake_case : nome_composto
+
+]]
