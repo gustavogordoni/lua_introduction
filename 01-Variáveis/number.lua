84b9e113eebe28dcedf03433f8223a69fdab27cb
Gustavo Luiz Gordoni <gustavogordoni1@gmail.com>
2025-08-17 18:12:46 -0300

    05-number

---
+++ 01-Variáveis/number.lua
@@ -0,0 +1,33 @@
+print("\nprint(type(3)): \t", type(3))
+print("print(type(3.2)): \t", type(3.2))
+
+x = 10
+print("\nx = '10'", "\nprint(x): \t\t", type(x))
+
+a = 10
+b = 20.2
+total = a + b
+
+print("\na = 10\nb = 20.2\ntotal = a + b")
+print("print(total): \t\t", total)
+
+------------------------------------------
+print([[
+]],"\n", [[--------- OP. ARITMÉTICOS ---------]])
+-- + ADIÇÂO
+print("print(20 + 5)\t\t", 20+5)
+
+-- - SUBTRAÇÂO
+print("print(20 - 5)\t\t", 20-5)
+
+-- / DIVISÂO
+print("print(20 / 5)\t\t", 20/5)
+
+-- * MULTIPLICAÇÂO
+print("print(20 * 5)\t\t", 20*5)
+
+-- % RESTO DA DIVISÂO INTEIRA
+print("print(20 % 5)\t\t", 20%5)
+
+-- ^ POTENCIAÇÃO
+print("print(20 ^ 5)\t\t", 20^5)
